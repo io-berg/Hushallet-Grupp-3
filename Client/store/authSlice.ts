1ce9cffdd88b37b2b5962011545bbfc6d8615b1a
berg <berg.iodaniel@gmail.com>
2022-10-14 15:56:58 +0200

    auth commentar

--- Client/store/authSlice.ts
+++ Client/store/authSlice.ts
@@ -8,16 +8,17 @@ export interface AuthState {
 }
 
 const initialState: AuthState = {
-  //   user: {
-  //     id: 0,
-  //     username: "me",
-  //     email: "email@email.com",
-  //   },
-  //   token: "1234567890",
-  //   expirationDate: new Date(9999, 12, 31).toISOString(),
-  user: null,
-  token: null,
-  expirationDate: null,
+  user: {
+    id: 0,
+    username: "me",
+    email: "email@email.com",
+  },
+  token: "1234567890",
+  expirationDate: new Date(9999, 12, 31).toISOString(),
+  // FÖR ATT SE LOGIN, ÄNDRA TILL NULL
+  //   user: null,
+  //   token: null,
+  //   expirationDate: null,
 };
 
 const authSlice = createSlice({
