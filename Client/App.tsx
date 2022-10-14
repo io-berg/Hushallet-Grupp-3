6cdd6424e024341eb280cab86124f367a27494e6
Linus Nilsson <linus.erik.nilsson@outlook.com>
2022-10-14 13:59:21 +0200

    Installed Safe Area provider

--- Client/App.tsx
+++ Client/App.tsx
@@ -4,13 +4,16 @@ import { Provider as PaperProvider } from "react-native-paper";
 import { Provider as ReduxProvider } from "react-redux";
 import { RootNavigator } from "./navigation/RootNavigator";
 import { store } from "./store/store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <ReduxProvider store={store}>
       <PaperProvider>
-        <StatusBar />
-        <RootNavigator />
+        <SafeAreaProvider>
+          <StatusBar />
+          <RootNavigator />
+        </SafeAreaProvider>
       </PaperProvider>
     </ReduxProvider>
   );
