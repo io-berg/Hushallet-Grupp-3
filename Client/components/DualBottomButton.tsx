5e2cd466da84f9582a88750b152aed9917c50e01
Linus Nilsson <linus.erik.nilsson@outlook.com>
2022-10-18 11:28:24 +0200

    test dual props for button

--- Client/components/DualBottomButton.tsx
+++ Client/components/DualBottomButton.tsx
@@ -2,19 +2,33 @@ import React from "react";
 import { Button, Text } from "react-native-paper";
 
 interface Props {
-  onPress: () => void;
-  text: string;
-  icon?: string;
-  disabled?: boolean;
+  onPress1: () => void;
+  text1: string;
+  icon1?: string;
+  disabled1?: boolean;
+
+  onPress2: () => void;
+  text2: string;
+  icon2?: string;
+  disabled2?: boolean;
 }
 
-const DualBottomButton = ({ onPress, text, icon, disabled }: Props) => {
+const DualBottomButton = ({
+  onPress1,
+  text1,
+  icon1,
+  disabled1,
+  onPress2,
+  text2,
+  icon2,
+  disabled2,
+}: Props) => {
   return (
     <view>
       <Button
-        onPress={onPress}
-        icon={icon}
-        disabled={disabled}
+        onPress={onPress1}
+        icon={icon1}
+        disabled={disabled1}
         mode="elevated"
         style={{
           width: "50%",
@@ -29,7 +43,28 @@ const DualBottomButton = ({ onPress, text, icon, disabled }: Props) => {
             fontSize: 18,
           }}
         >
-          {text}
+          {text1}
+        </Text>
+      </Button>
+      <Button
+        onPress={onPress2}
+        icon={icon2}
+        disabled={disabled2}
+        mode="elevated"
+        style={{
+          width: "50%",
+          shadowColor: "black",
+          backgroundColor: "#ffff",
+          elevation: 5,
+        }}
+      >
+        <Text
+          style={{
+            color: "black",
+            fontSize: 18,
+          }}
+        >
+          {text2}
         </Text>
       </Button>
       ;
