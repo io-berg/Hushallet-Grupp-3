c24f0ab838c70dbd96ab75684609926bdb6705ae
Linus Nilsson <linus.erik.nilsson@outlook.com>
2022-10-18 08:54:58 +0200

    initial props for button set

--- Client/components/DualBottomButton.tsx
+++ Client/components/DualBottomButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Text } from "react-native-paper";
 
-interface props {
+interface Props {
   onPress: () => void;
   text: string;
   icon?: string;
@@ -9,7 +9,32 @@ interface props {
 }
 
 const DualBottomButton = ({ onPress, text, icon, disabled }: Props) => {
-  return <Button></Button>;
+  return (
+    <view>
+      <Button
+        onPress={onPress}
+        icon={icon}
+        disabled={disabled}
+        mode="elevated"
+        style={{
+          width: "50%",
+          shadowColor: "black",
+          backgroundColor: "#ffff",
+          elevation: 5,
+        }}
+      >
+        <Text
+          style={{
+            color: "black",
+            fontSize: 18,
+          }}
+        >
+          {text}
+        </Text>
+      </Button>
+      ;
+    </view>
+  );
 };
 
 export default DualBottomButton;
