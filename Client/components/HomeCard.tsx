87433bdb16f558a0b93a91a3b054a55c7d9f3e9c
Eva-Li8E <evaliboden@gmail.com>
2022-10-28 09:46:06 +0200

    fixa margin

--- Client/components/HomeCard.tsx
+++ Client/components/HomeCard.tsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     shadowColor: "#333",
     shadowOpacity: 0.3,
     shadowRadius: 2,
-    marginHorizontal: 4,
+    marginHorizontal: 10,
     marginVertical: 6,
   },
   cardcontent: {
